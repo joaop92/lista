f9245197fbc5cfe31398a46673161713f5618ea3
joaop92 <75449544+joaop92@users.noreply.github.com>
2023-08-01 20:06:44 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+
+
+
+const nomes = ["João", "Maria", "José", "Pedro", "Ana", "Luís", "Fernanda", "Rafael", "Isabel", "Guilherme"];
+const idades = [20, 25, 30, 35, 40, 45, 50, 55, 60, 65];
+const cores = ["azul", "verde", "vermelho", "amarelo", "laranja", "roxo", "rosa", "cinza", "preto", "branco"];
+
+// Imprime as listas
+console.log("Nomes:", nomes);
+console.log("Idades:", idades);
+console.log("Cores:", cores);
+
+// Faz algumas modificações, alterando uma cor e uma idade
+nomes[1] = "Mariana";
+idades[3] = 31;
+cores[5] = "azul-claro";
+
+// Imprime novamente todas as informações das três listas
+console.log("Nomes:", nomes);
+console.log("Idades:", idades);
+console.log("Cores:", cores);
+
+
+
